419b0376d2704980dfcc4413e303d68dea800428
Salman Alameer <salmanalameer2@gmail.com>
2024-12-16 13:05:37 +0800

    ز

--- frontend/src/app/components/MainContent.tsx
+++ frontend/src/app/components/MainContent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FolderContainer from './folderContainer';
 import DailyTasks from './DailyTasks';
-import WeeklyTimetable from './timetable';
+import Timetable from './timetable';
 
 // Define the folders array with the correct type
 const folders: { name: string; color: 'red' | 'blue' | 'yellow' | 'green' }[] = [
@@ -9,6 +9,22 @@ const folders: { name: string; color: 'red' | 'blue' | 'yellow' | 'green' }[] = 
   { name: 'System Design', color: 'blue' },
   { name: 'Internet Programming', color: 'yellow' },
   { name: 'Web Development', color: 'green' },
+];
+
+const classes = [
+  { day: 'Monday', time: '8:00 AM - 10:00 AM', className: 'App Development' },
+  { day: 'Tuesday', time: '10:00 AM - 12:00 PM', className: 'System Design' },
+  { day: 'Wednesday', time: '12:00 PM - 2:00 PM', className: 'Internet Programming' },
+  { day: 'Thursday', time: '2:00 PM - 4:00 PM', className: 'Web Development' },
+  { day: 'Friday', time: '4:00 PM - 5:00 PM', className: 'System Design' },
+];
+const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+const timeSlots = [
+  '8:00 AM - 10:00 AM',
+  '10:00 AM - 12:00 PM',
+  '12:00 PM - 2:00 PM',
+  '2:00 PM - 4:00 PM',
+  '4:00 PM - 5:00 PM',
 ];
 
 export default function MainContent() {
@@ -24,7 +40,7 @@ export default function MainContent() {
       </div>
 
       <div className="weekly-timetable">
-        <WeeklyTimetable />
+      <Timetable classes={classes} daysOfWeek={daysOfWeek} timeSlots={timeSlots} />
       </div>
     </div>
   );
